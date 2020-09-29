c8d03aded19fec9cf07b52cd38fd720b657b79f5
luke-h1 <luke.howsam@yahoo.com>
2020-09-29 12:26:04 +0100

    init commit

---
+++ app.js
@@ -0,0 +1,53 @@
+const form = document.getElementById('form');
+const userQuery = document.getElementById('userQuery');
+const outputDiv = document.getElementById('grid');
+function showError(message) {
+  const h1 = document.getElementById('error');
+  h1.innerHTML = message;
+  setTimeout(() => {
+    h1.innerHTML = '';
+  }, 2000);
+}
+
+async function fetchRedditData(e) {
+  e.preventDefault();
+  if (userQuery.value === '') {
+    showError('Enter a correct value');
+  } else {
+    const QUERY = userQuery.value;
+    const API_URL = `https://www.reddit.com/search.json?q=${QUERY}`;
+    const res = await fetch(`${API_URL}`);
+    const data = await res.json();
+    console.log(data);
+    showRedditData(data);
+  }
+}
+
+function truncateText(text, limit) {
+  const shortended = text.indexOf(' ', limit);
+  if (shortended === -1) return text;
+  return text.substring(0, shortended);
+}
+
+function showRedditData(data) {
+  let output = '';
+  data.data.children.map((item) => {
+    output += `
+    <div class="card">
+    <div class="card-img-container">
+      <img src="${item.data.thumbnail}" alt="">
+    </div>
+    <p> ${item.data.title}</p>
+    <p>Subreddit: ${item.data.subreddit}</p>
+    <div class="badge-container">
+      <span class="badge badge-primary">Votes: ${item.data.ups}</span>
+      <span class="badge badge-success">Score: ${item.data.score}</span>
+    </div>
+  </div>
+    `;
+  });
+  outputDiv.innerHTML = output;
+}
+
+// EVENT LISTENERS
+form.addEventListener('submit', fetchRedditData);
